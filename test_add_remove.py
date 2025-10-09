0ef24430217c62dd8f3e7a008beb43697cd047a0
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-09 12:47:05 -0500

    Updated test_add_remove function for click actions

--- test_add_remove.py
+++ test_add_remove.py
@@ -6,3 +6,13 @@ def test_add_remove_button(page):
 
     page_header = page.locator("h3")
     assert "Add/Remove" in page_header.inner_html()
+
+    add_button = page.locator("text=Add Element")
+    # clicking the add button 5 times
+    for _ in range(5):
+        add_button.click()
+
+    # Finding all the delete buttons
+    delete_buttons = page.locator("button.added-manually")
+    count = delete_buttons.count()
+    assert count == 5
