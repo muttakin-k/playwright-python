6018975a19c146c0cdfac72191e0488d5b3ffdaa
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-09 14:00:24 -0500

    Wrote a new method for testing broken images

--- test_ab.py
+++ test_ab.py
@@ -1,9 +1,13 @@
 def test_ab(page):
-    #print("Page Title is ----->"+page.title())
-
     locator = page.locator("text=A/B Testing")
     locator.click()
     page_header = page.locator("h3")
     header_text = page_header.text_content()
     assert "A/B" in header_text
-    print(header_text)
+
+def test_broken_images(page):
+    locator = page.locator("text=Broken Images")
+    locator.click()
+    page_header = page.locator("h3")
+    header_text = page_header.text_content()
+    assert "Broken Images" in header_text
