4177e848fb647a88124077e2e72d909903238b6f
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-11 02:11:51 -0500

    Updated test challenging DOM section

--- test_DOM.py
+++ test_DOM.py
@@ -1,2 +1,22 @@
+from conftest import take_screenshot
+
 def test_challenging_dom(page):
-    pass
+    locator = page.locator("text=Challenging DOM")
+    locator.click()
+
+    page_header = page.locator("h3")
+    assert "Challenging" in page_header.inner_html()
+
+    blue_button = page.locator("button.button")
+    red_button = page.locator("button.button.alert")
+    green_button = page.locator("button.button.success")
+
+    blue_button.click()
+    take_screenshot(page, "blue_button_clicked")
+
+    red_button.click()
+    take_screenshot(page, "red_button_clicked")
+
+    green_button.click()
+    take_screenshot(page, "green_button_clicked")
+
