422850f144e3f8d638d87afb833ccf7f9d2acd3e
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-13 23:18:26 -0500

    Updated testing digest auth to ensure the stateful authentication

--- test_digest_auth.py
+++ test_digest_auth.py
@@ -1,6 +1,13 @@
 def test_digest_auth(login_page):
+    # Initial navigation and assertion to digest auth
     login_page.goto("https://the-internet.herokuapp.com/digest_auth")
     content = login_page.locator("p").text_content()
     print("Page content:", content)
     assert "Congratulations" in content
 
+    # Digest auth offers statefullness, so testing that here
+    login_page.go_back()
+    # final navigation to digest auth and testing if we are still logged in
+    login_page.goto("https://the-internet.herokuapp.com/digest_auth")
+    print("Page content:", content)
+    assert "proper credentials" in content
