f58574475ce0786225b33322504f2fff9129d56f
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-14 22:08:18 -0500

    Added a new file for testing drag drop

---
+++ test_drag_drop.py
@@ -0,0 +1,16 @@
+def test_drag_drop(page):
+    locator = page.locator("text=Drag and Drop")
+    locator.click()
+
+    source = page.locator("#column-a")
+    dest = page.locator("#column-b")
+
+    source.drag_to(dest)
+
+    header_a = page.locator("#column-a header").inner_text()
+    header_b = page.locator("#column-b header").inner_text()
+
+    print("\n")
+
+    print(f"After drag: A={header_a}, B={header_b}")
+    assert header_a == "B" and header_b == "A"
