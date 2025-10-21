0821a4e2557511895a4e27d866bc4f92dbc761b4
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-21 15:16:49 -0500

    Updated dropdown test script

--- test_dropdown.py
+++ test_dropdown.py
@@ -1,5 +1,18 @@
 def test_dropdown(page):
-    pass
+    locator = page.locator("text=Dropdown")
+    locator.click()
 
-    # new line
-    
+    # Locating all the checkboxes
+    dd_locator = page.locator("#dropdown")
+
+    # Select by value
+    page.select_option("#dropdown", value = "1")
+
+    # asserting if the value is 1
+    assert dd_locator.input_value() == "1"
+
+    # Select by label
+    page.select_option("#dropdown", label = "Option 2")
+
+    # asserting if the value is 2
+    assert dd_locator.input_value() == "2"
