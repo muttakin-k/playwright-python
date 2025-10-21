9b4345c5b71bccc6ff0ecb760e7e7ee4d173580e
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-21 15:19:20 -0500

    updated dropdown test script to test selecgt by index

--- test_dropdown.py
+++ test_dropdown.py
@@ -16,3 +16,9 @@ def test_dropdown(page):
 
     # asserting if the value is 2
     assert dd_locator.input_value() == "2"
+
+    # Select by index
+    page.select_option("#dropdown", index = 1)
+
+    # asserting if the value is 1
+    assert dd_locator.input_value() == "1"
