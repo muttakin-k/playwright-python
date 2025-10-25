279d20aa9ecc1738a14a5b3d8f7506cc98d0e7c3
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-24 19:45:19 -0500

    Updated test script for dynamic controls: add/remove

--- test_dynamic_controls.py
+++ test_dynamic_controls.py
@@ -1,2 +1,12 @@
 def test_dynamic_controls(page):
-    pass
+    locator = page.locator("text=Dynamic Controls")
+    locator.click()
+
+    # First testing the add/remove button and asserting the correct result
+    page.locator("text=Remove").click()
+    message = page.locator("#message")
+    assert message == "It's gone!"
+
+    page.locator("text=Add").click()
+    message = page.locator("#message")
+    assert message == "It's back!"
