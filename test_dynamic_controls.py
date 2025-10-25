8d061a8c398d8bdc8d19ade7ac9e17a145550bac
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-24 19:54:33 -0500

    Bug fixing test dynamic controls: add/remove

--- test_dynamic_controls.py
+++ test_dynamic_controls.py
@@ -3,11 +3,16 @@ def test_dynamic_controls(page):
     locator.click()
 
     # First testing the add/remove button and asserting the correct result
+    # At initial stage there will be the remove button but no text in #message
+    message_locator = page.locate("#message")
+    assert message_locator.count() == 0
+
+    # Clicking the remove button for the first time
     page.get_by_role("button", name="Remove").click()
     message = page.locator("#message")
     assert message.inner_text() == "It's gone!"
 
+    # Clicking on the add button for the first time
     page.get_by_role("button", name="Add").click()
-    #page.locator("text=Add").click()
     message = page.locator("#message")
     assert message.inner_text() == "It's back!"
