0c718ff61b6f611c075c04bc65c5979eb90141b1
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-26 13:48:48 -0500

    Updated test dynamic controls: testing enable/disable - added some wait times

--- test_dynamic_controls.py
+++ test_dynamic_controls.py
@@ -22,15 +22,19 @@ def test_dynamic_controls(page):
 
     # Second Phase of testing: Testing the textbox
     # The textbox should be disabled at first
-    text_box = page.get_by_role("input", name="Enable")
+    text_box = page.locator("input[type='text']")
     assert text_box.is_disabled()
 
     # Now clicking the button "Enable" and assert again
-    page.get_by_role("button", text="Enable").click()
-    disable_button = page.get_by_role("button", text="Disable")
+    page.get_by_role("button", name="Enable").click()
+    page.wait_for_timeout(10000)
+    text_box = page.locator("input[type='text']")
     assert text_box.is_enabled()
+
+    disable_button = page.get_by_role("button", name="Disable")
 
     # Clicking the disable button and asserting
     disable_button.click()
+    page.wait_for_timeout(10000)
     assert text_box.is_disabled()
 
