961399991494a5ae8edf692e9c9730f551d4c030
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-26 02:03:07 -0500

    Updated test dynamic controls: testing enable/disable

--- test_dynamic_controls.py
+++ test_dynamic_controls.py
@@ -16,6 +16,21 @@ def test_dynamic_controls(page):
     # At this time, the message is supposed to be there so asserting
     message_locator = page.locator("#message")
     assert message_locator.count() == 1
-    page.get_by_role("button", name="Add").click()
+    page.get_by_role("button", ).click()
     message = page.locator("#message")
     assert message.inner_text() == "It's back!"
+
+    # Second Phase of testing: Testing the textbox
+    # The textbox should be disabled at first
+    text_box = page.get_by_role("input", name="Enable")
+    assert text_box.is_disabled()
+
+    # Now clicking the button "Enable" and assert again
+    page.get_by_role("button", text="Enable").click()
+    disable_button = page.get_by_role("button", text="Disable")
+    assert text_box.is_enabled()
+
+    # Clicking the disable button and asserting
+    disable_button.click()
+    assert text_box.is_disabled()
+
