29e22c7859c7f43b24b62f8b8b76d6d8682c3461
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-27 22:43:29 -0500

    Updated test dynamic loading: example 1 success

--- test_dynamic_loading.py
+++ test_dynamic_loading.py
@@ -3,17 +3,31 @@ def dl1(p):
     h4_locator = p.locator("h4").nth(0)
     assert "Example 1:" in h4_locator.inner_html()
 
+    # clicking the button 'Start'
+    p.get_by_role("button", name="Start").click()
+
+    # Waiting until hello worls is visible
+    p.wait_for_selector("#finish", state="visible")
+
+    # Asserting
+    div = p.locator("#finish")
+    hw = div.locator('h4')
+    assert hw.inner_text() == "Hello World!"
+
+
 def dl2(p):
     p.get_by_role("link", name="Example 2", exact=False).click()
     h4_locator = p.locator("h4").nth(0)
     assert "Example 1:" not in h4_locator.inner_html()
+
+
 
 def test_dynamic_loading(page):
     locator = page.locator("text=Dynamic Loading")
     locator.click()
 
     # Testing the first example: Dynamic Loading 1
-    #dl1(page)
+    dl1(page)
 
     # Testing the first example: Dynamic Loading 2
-    dl2(page)
+    #dl2(page)
