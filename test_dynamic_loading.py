9240245b1ccac109d1e273519f70e42d244e5479
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-10-27 22:34:02 -0500

    Updated test dynamic loading: example 2

--- test_dynamic_loading.py
+++ test_dynamic_loading.py
@@ -3,9 +3,17 @@ def dl1(p):
     h4_locator = p.locator("h4").nth(0)
     assert "Example 1:" in h4_locator.inner_html()
 
+def dl2(p):
+    p.get_by_role("link", name="Example 2", exact=False).click()
+    h4_locator = p.locator("h4").nth(0)
+    assert "Example 1:" not in h4_locator.inner_html()
+
 def test_dynamic_loading(page):
     locator = page.locator("text=Dynamic Loading")
     locator.click()
 
     # Testing the first example: Dynamic Loading 1
-    dl1(page)
+    #dl1(page)
+
+    # Testing the first example: Dynamic Loading 2
+    dl2(page)
