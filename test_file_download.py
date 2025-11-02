6a3a3e8112cb2cd24b12b3c01b4f0dad56c57040
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-11-01 23:33:34 -0500

    updated test file download

--- test_file_download.py
+++ test_file_download.py
@@ -1,2 +1,12 @@
+import os
+
 def test_file_download(page):
-    pass
+    locator = page.locator("text=File Download").first
+    locator.click()
+
+    intended_file_name = "bb.txt"
+    file_locator = page.locator("text="+intended_file_name)
+    file_locator.click()
+
+    downloads_path = "C:/Users/user/Downloads/"
+    assert os.path.basename(downloads_path+intended_file_name) == intended_file_name
