160738aa613939b75de6ab20ca0cacfed5fe8406
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-11-02 18:52:19 -0600

    updated test file upload

--- test_file_upload.py
+++ test_file_upload.py
@@ -1,2 +1,14 @@
 def test_file_upload(page):
-    pass
+    page.goto("https://the-internet.herokuapp.com/upload")
+
+    choose_file_button = page.locator("input#file-upload")
+    #choose_file_button.click()
+
+    file_path = "C:/Users/user/Downloads/"
+    file_name = "images.jpeg"
+    choose_file_button.set_input_files(file_path+file_name)
+
+    page.locator("input#file-submit").click()
+
+    uploaded_file_location = page.locator("div#uploaded-files")
+    assert uploaded_file_location.inner_text() == file_name
