52dce7f25cd34b5a406887ff45be58eb65928238
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-11-08 21:32:28 -0600

    updated test floating menu

--- test_floating_menu.py
+++ test_floating_menu.py
@@ -1,2 +1,16 @@
+from helper_functions import take_screenshot
+
 def test_floating_menu(page):
-    assert True == 1
+    page.goto("https://the-internet.herokuapp.com/floating_menu")
+
+    # Scrolling down to 1000 px and take a screenshot
+    page.mouse.wheel(0, 1000)
+    take_screenshot(page, "floating_menu_down_1000px")
+
+    # Scrolling to the bottom of the page and taking a screenshot
+    page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
+    take_screenshot(page, "floating_menu_page_bottom")
+
+    # Scrolling up to 1000 px and take a screenshot
+    page.mouse.wheel(0, -1000)
+    take_screenshot(page, "floating_menu_up_1000px")
