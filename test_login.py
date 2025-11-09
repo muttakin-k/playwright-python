a065feaca50483ddb4fa93caf17785bfc6788051
muttakin-ahmed <muttakinahmedkhan@gmail.com>
2025-11-08 22:19:09 -0600

    Updated test authentication

--- test_login.py
+++ test_login.py
@@ -1,2 +1,23 @@
 def test_authentication(page):
-    pass
+    page.goto("https://the-internet.herokuapp.com/login")
+
+    username = page.locator("#username")
+    passowrd = page.locator("#password")
+    submit_btn = page.locator("button[type='submit']")
+
+    valid_user = "tomsmith"
+    valid_pass = "SuperSecretPassword!"
+
+    invalid_user = "t"
+    invalid_pass = "SSecretPassword!"
+
+    login_error = page.locator("#flash")
+
+    # testing with invalid user-pass combination
+    username.fill(invalid_user)
+    passowrd.fill(invalid_pass)
+    submit_btn.click()
+
+    # Asserting the error msg
+    assert login_error.inner_text() == "Your username is invalid!"
+
